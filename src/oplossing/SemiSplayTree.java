f2e33104169f3602fa9973208838d1848e1b5fea
Elias Nijs <elias.nijs@gmail.com>
2021-10-20 21:14:02 +0200

    semisplay iterator draft 5

--- src/oplossing/SemiSplayTree.java
+++ src/oplossing/SemiSplayTree.java
@@ -22,6 +22,7 @@ public class SemiSplayTree<E extends Comparable<E>> implements SearchTree<E> {
 
     @Override
     public boolean search(E o) {
+        System.out.println("SEARCHING");
         return root != null && searchHelper(o, root);
     }
 
